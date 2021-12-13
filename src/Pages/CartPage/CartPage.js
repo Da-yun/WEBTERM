c613d9ef6cd3582411af372909cb66abbac24bd1
Da-yun <dnfekfdl@o.cnu.ac.kr>
2021-12-13 21:52:58 +0900

    [#4] cart 수정중

--- src/Pages/CartPage/CartPage.js
+++ src/Pages/CartPage/CartPage.js
@@ -1,49 +1,68 @@
 import '../CartPage/cartstyle.css';
-import React from 'react';
-import { useTable } from 'react-table';
-import Data from '../CartPage/Data'; // 테이블 cell에 들어갈 데이터파일입니다.
-import Column from '../CartPage/Coulmn'; // 테이블 column 데이터 파일입니다.
+import React, { useEffect } from 'react';
+import ramen from '../../Image/gimchi/ramen.png';
+import gimchi from '../../Image/gimchi/gimchi.png';
+import boodae from '../../Image/gimchi/boodae.png';
+import oogugi from '../../Image/gimchi/oogugi.png';
+import $ from 'jquery';
 
 // 장바구니 클릭 시 이동하는 장바구니 화면입니다.
 // react 라이브러리인 react-table을 사용하여 테이블을 구성합니다.
 // 아직 데이터 작업이 이루어지지 않아 테이블 컴포넌트만 생성한 상태입니다.
 function CartPage() {
-  const data = Data();
-  const columns = Column();
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ columns, data });
+  const img = [
+    '<img src = ' + ramen + '/>',
+    '<img src = ' + ramen + '/>',
+    '<img src = ' + ramen + '/>',
+  ];
+  function handler(e) {
+    if (e.target.id === 'IMG') {
+      let vi = $(e.target.parentNode);
+      vi.css('animation-name', 'example');
+      vi.css('animation-duration', '4s');
+      console.log(vi);
+      //vi.setPropterty('animation-name', 'example');
+      //e.target.parentNode
+      //e.target.parentNode.css(' animation-duration', '4s');
+    }
+  }
+  useEffect(() => {
+    var item1 = $('#imgContainer');
+    document.addEventListener('mousemove', handler);
+    item1.mouseenter(function (e) {
+      item1.animate({ width: '+=150px' });
+    });
+    item1.click(function () {});
+    item1.mouseleave(function (e) {});
+  });
   return (
     <div className="container">
-      <p>장바구니</p>
-      <div className="table">
-        <table {...getTableProps()}>
-          <thead>
-            {headerGroups.map((headerGroup) => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column) => (
-                  <th {...column.getHeaderProps()}>
-                    {column.render('Header')}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody {...getTableBodyProps()}>
-            {rows.map((row) => {
-              prepareRow(row);
-              return (
-                <tr {...row.getRowProps()}>
-                  {row.cells.map((cell) => {
-                    return (
-                      <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                    );
-                  })}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-        <button>결제하기</button>
+      <p id="cart">수집한 카드</p>
+      <div className="cotainer2">
+        <div id="img1">
+          <img id="IMG" src={ramen}></img>
+        </div>
+        <div id="imgcontainer">
+          <img id="IMG" src={gimchi}></img>
+        </div>
+        <div id="imgcontainer">
+          <img id="IMG" src={boodae}></img>
+        </div>
+        <div id="imgcontainer">
+          <img id="IMG" src={oogugi}></img>
+        </div>
+        <div id="imgcontainer">
+          <img id="IMG" src={oogugi}></img>
+        </div>
+        <div id="imgcontainer">
+          <img d="IMG" src={oogugi}></img>
+        </div>
+        <div id="imgcontainer">
+          <img id="IMG" src={oogugi}></img>
+        </div>
+        <div id="imgcontainer">
+          <img id="IMG" src={oogugi}></img>
+        </div>
       </div>
     </div>
   );
